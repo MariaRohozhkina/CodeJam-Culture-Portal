20ae3d784cc2cae5c59168595de0f13fb169745c
Denis <densert666@gmail.com>
2020-02-18 20:23:43 +0300

    feat: add routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({children}) => (
+    {children}
+);
+
+export default App;
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <Switch>
+                <Route exact path='/' render={()=><MainPage />} />
+                <Route path='/authors' render={()=><AuthorsList />} />
+                <Route path='/team' render={()=><TeamPage />} />
+                <Route path='/worklog' render={()=><Worklog />} />
+                <Route path='/styleguide' render={()=><StyleGuide />} />
+            </Switch>
+        </App>
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
