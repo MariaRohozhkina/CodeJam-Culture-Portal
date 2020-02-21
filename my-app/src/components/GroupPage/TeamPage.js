137e2f1f1de0d7c9fd1fc09317c512a5a7676e97
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-02-21 22:21:42 +0600

    feat: add styles and mock data to team cards

--- my-app/src/components/GroupPage/TeamPage.js
+++ my-app/src/components/GroupPage/TeamPage.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import './TeamPage.scss';
+import { teamInfo } from './teamInfo';
 
 export default function TeamPage() {
   return (
-    <>
-      <div className="team-page">
-        <div className="row row-cols-1 row-cols-md-2">
-          <div className="col mb-4">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+    <div className="team-page">
+      <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
+        {teamInfo.map((el, i) => {
+          return (
+            <div className="col mb-3" key={i}>
+              <div className="card team-card text-center">
+                <img
+                  src={el[0].avatar}
+                  className="card-img-top team-avatar"
+                  alt="avatar"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{el[0].name}</h5>
+                  <p className="card-text">{el[0].info}</p>
+                  <a
+                    href={el[0].github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-primary"
+                  >
+                    Github Profile
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
---
+++ my-app/src/components/GroupPage/teamInfo.js
@@ -0,0 +1,65 @@
+import denisAva from './avatars/denis.png';
+import daniyarAva from './avatars/daniyar.jpeg';
+import alexAva from './avatars/alex.png';
+import irisAva from './avatars/iris.png';
+import kseniyaAva from './avatars/kseniya.jpeg';
+import mariaAva from './avatars/maria.jpeg';
+
+const teamInfo = [
+  [
+    {
+      name: 'Daniyar Ketebay',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/luckytiger1',
+      avatar: daniyarAva,
+    },
+  ],
+  [
+    {
+      name: 'Denis Arkhipov',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/VikinGSpb',
+      avatar: denisAva,
+    },
+  ],
+  [
+    {
+      name: 'Ksenia Yatskevich',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/KseniyaYatskevich',
+      avatar: kseniyaAva,
+    },
+  ],
+  [
+    {
+      name: 'Maria Rogozhkina',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/MariaRohozhkina',
+      avatar: mariaAva,
+    },
+  ],
+  [
+    {
+      name: 'Aliaksandra Mikhalchanka',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/Alex89889',
+      avatar: alexAva,
+    },
+  ],
+  [
+    {
+      name: 'Iris Shoi',
+      info:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam facere, quos assumenda qui maiores in!',
+      github: 'https://github.com/missirisshoi',
+      avatar: irisAva,
+    },
+  ],
+];
+
+export { teamInfo };
