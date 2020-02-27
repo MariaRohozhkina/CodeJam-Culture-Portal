09755da787e7e7465175c0fe43cda9167806db0b
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-02-28 00:24:17 +0600

    fix: fix styles of group page

--- my-app/src/components/GroupPage/TeamPage.js
+++ my-app/src/components/GroupPage/TeamPage.js
@@ -5,32 +5,41 @@ import { teamInfo } from './teamInfo';
 export default function TeamPage() {
   return (
     <div className="team-page">
-      <h2 className="team-header">
+      <h2 className="team-header text-center">
         Team members that made contributions to this project
       </h2>
       <hr />
-      <div className="card-group">
-        {teamInfo.map((el, i) => (
-          <div className="col-sm-3" key={i}>
-            <div className="card team-card text-center">
-              <img
-                src={el[0].avatar}
-                className="card-img-top team-avatar"
-                alt="avatar"
-              />
-              <div className="card-body">
-                <h4 className="card-title team-card-title">{el[0].name}</h4>
-                <p className="card-text">{el[0].info}</p>
-                <a
-                  href={el[0].github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="team-github-link"
-                ></a>
+      <div className="container">
+        <div className="card-group">
+          {teamInfo.map((el, i) => (
+            <div
+              className="col-sm-9 col-md-4 col-10"
+              key={i}
+              style={{ marginBottom: '30px' }}
+            >
+              <div className="card team-card text-center">
+                <div>
+                  <img
+                    src={el[0].avatar}
+                    className="card-img-top team-avatar"
+                    alt="avatar"
+                    style={{ objectFit: 'contain' }}
+                  />
+                </div>
+                <div className="card-body">
+                  <h4 className="card-title team-card-title">{el[0].name}</h4>
+                  <p className="card-text">{el[0].info}</p>
+                  <a
+                    href={el[0].github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="team-github-link"
+                  ></a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- my-app/src/components/mainPage/main.js
+++ my-app/src/components/mainPage/main.js
@@ -1,48 +1,51 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 
-import "./main.css";
+import './main.css';
 
-import Description from "./description";
+import Description from './description';
 import photographersData from '../../assets/photographersData';
 
 class MainPage extends Component {
-
   chooseDate() {
     const date = new Date();
     const numOfTheAuthor = date.getDay();
     return numOfTheAuthor;
   }
 
-    render() {
-      const nameOfRubric = 'Автор дня'; 
-      const nameOfAuthor = photographersData[this.chooseDate()].RU.name;
-      const summary = photographersData[this.chooseDate()].RU.summary;
-      const photo = photographersData[this.chooseDate()].photo;
-      const learnMoreBtn = 'Узнать больше »';
+  render() {
+    const nameOfRubric = 'Автор дня';
+    const nameOfAuthor = photographersData[0].RU.name;
+    const summary = photographersData[0].RU.summary;
+    const photo = photographersData[0].photo;
+    const learnMoreBtn = 'Узнать больше »';
 
-      return (
-        <Fragment>
-          <div className="jumbotron">
-            <div>
-              <Description />
-            </div>
+    return (
+      <Fragment>
+        <div className="jumbotron">
+          <div>
+            <Description />
           </div>
-          <div className="col-md-4">
-            <div>
-              <img className="author-photo-container" src={photo} alt="Photographer"></img>
-            </div>
-            <div id='info-about-author'>
-              <h2>{nameOfRubric}</h2>
-              <h5>{nameOfAuthor}</h5> 
-              <p>{summary}</p>
-              <p className='border'>
-                <button className="btn btn-default">{learnMoreBtn}</button>
-              </p>
-            </div>
+        </div>
+        <div className="col-md-4">
+          <div>
+            <img
+              className="author-photo-container"
+              src={photo}
+              alt="Photographer"
+            ></img>
           </div>
-        </Fragment>
-      );
-    }
+          <div id="info-about-author">
+            <h2>{nameOfRubric}</h2>
+            <h5>{nameOfAuthor}</h5>
+            <p>{summary}</p>
+            <p className="border">
+              <button className="btn btn-default">{learnMoreBtn}</button>
+            </p>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
-  
-  export default MainPage;
+}
+
+export default MainPage;
