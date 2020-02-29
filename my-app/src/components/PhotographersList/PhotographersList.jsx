5f0e5dec372e397e434f299af55220a2a6edb9e9
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-02-29 18:23:02 +0600

    feat: add search widget to authors page

--- my-app/src/components/PhotographersList/PhotographersList.jsx
+++ my-app/src/components/PhotographersList/PhotographersList.jsx
@@ -1,30 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PhotographerCard from '../PhotographerCard/PhotographerCard';
 import photographersData from '../../assets/photographersData';
 import { TRANSLATE } from '../../assets/translate';
 import './PhotographersList.css';
 
+function PhotographersList({ lang }) {
+  const [term, setTerm] = useState('');
 
+  const handleSearch = () => {
+    const inputValue = document.querySelector('.search-input');
+    setTerm(inputValue.value);
+  };
 
-function PhotographersList({ lang }) {  
-  return ( 
+  const searchByTerm = (items, searchTerm) => {
+    if (searchTerm === '') {
+      return items;
+    }
+    console.log(lang);
+    return items.filter((item) => {
+      return (
+        item[lang].name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 ||
+        item[lang].birthCity.toLowerCase().indexOf(searchTerm.toLowerCase()) >
+          -1
+      );
+    });
+  };
+
+  const visibleData = searchByTerm(photographersData, term, lang);
+
+  return (
     <section className="content">
-      <div className="wrapper">
-        {photographersData.map((card) => (
-        <PhotographerCard 
-          key = {card.id}
-          id = {card.id}
-          lang={lang}
-          photo= {card.photo}
-          years={card[lang].years}
-          name={card[lang].name}
-          shortDescription={card[lang].summary}
-          buttonContent={TRANSLATE[lang].buttonContent}
-        />
-        ))}    
-      
+      <div className="container">
+        <div className="search-box input-group mb-3">
+          <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Search for photographer"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-primary"
+              type="button"
+              onClick={handleSearch}
+            >
+              Button
+            </button>
+          </div>
+        </div>
       </div>
-    </section>    
+      <div className="wrapper">
+        {visibleData.map((card) => (
+          <PhotographerCard
+            key={card.id}
+            id={card.id}
+            lang={lang}
+            photo={card.photo}
+            years={card[lang].years}
+            name={card[lang].name}
+            shortDescription={card[lang].summary}
+            buttonContent={TRANSLATE[lang].buttonContent}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
 
